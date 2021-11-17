52faca763d19167da86cb92b65950587bb3e0f09
Peter Hu <any99147@gmail.com>
2021-11-17 18:05:08 +0800

    add redis

---
+++ lab/redisLab.py
@@ -0,0 +1,17 @@
+import redis
+
+REDIS_PASSWORD = "plZdYk4XnNWaS9GgRlVOWm-39ZqvWVAg-cawCdtZndBUNciDQ1"
+pool = redis.ConnectionPool(host='192.168.56.90', port=6379, decode_responses=True, password=REDIS_PASSWORD)
+r = redis.Redis(connection_pool=pool)
+
+def event_handler(msg):
+    print('Handler', msg)
+
+def subscribe():
+    pubsub = r.pubsub()
+    subscribe_key = '*'
+    pubsub.psubscribe(**{subscribe_key: event_handler})
+    pubsub.run_in_thread(sleep_time=.01)
+
+if __name__ == '__main__':
+    subscribe()
