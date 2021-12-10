cdc33d78f67ff2c4bc868f312405cb6b46e11b7a
Peter Hu <any99147@gmail.com>
2021-12-10 16:26:37 +0800

    cf

--- lab/requestsLab.py
+++ lab/requestsLab.py
@@ -6,3 +6,17 @@ projects = res.json()
 for i in range(0,len(projects)):
     print(projects[i]["ProjectName"])
     # print(projects[i].get("ProjectName"))
+
+# variables = '{"groupId": "R3JvdXA.YJ3ZKZYEDgAHH1MW"}'
+# requestBody = {
+#     'variables': variables, 
+#     'query': 'query ($groupId: ID!) { group(id: $groupId) { id name } }'
+# }
+# headers = {'X-Ifp-App-Secret': 'OWFhYThkZWEtOGFjZS0xMWViLTk4MzItMTZmODFiNTM3OTI4'}
+# res = requests.post("https://ifp-organizer-dmd-eks009.sa.wise-paas.com/graphql",data=requestBody,headers=headers)
+# print(res)
+# resJSON = res.json()
+# if 'errors' in resJSON :
+#     print(resJSON['errors'])
+# else :
+#     print(resJSON['data'])
