0ab9a0cb2639b1d562d1fb5a009b030105a945dd
TsengHao Hu <any99147@gmail.com>
2022-02-24 18:03:11 +0800

    cv

--- lab/timeLab.py
+++ lab/timeLab.py
@@ -1,5 +1,9 @@
 import time
+from datetime import datetime
 
 timeData = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 print(timeData)
 time.sleep(1)
+
+bd = datetime.strptime("1997-05-02 17:05:02", "%Y-%m-%d %H:%M:%S")
+print("Peter Birthday "+ str(bd))
