c83627de79c54db605187d09e7e266c5a5abfa63
TsengHao Hu <any99147@gmail.com>
2022-08-10 12:46:18 +1000

    update

--- lab/dictionaryLab.py
+++ lab/dictionaryLab.py
@@ -1,3 +1,9 @@
 if __name__ == '__main__':
-    dic = {"affordable": 0, "expensive": 3, "cheap": 5, "very expensive": 0}
+    dic = {"affordable": 0, "expensive": 3, "cheap": 5, "very expensive": 5}
     print(max(dic, key=dic.get))
+
+    predictionKeys = []
+    for key, value in dic.items():
+        if value == dic.get(max(dic, key=dic.get)):
+            predictionKeys.append(key)
+    print(predictionKeys)
